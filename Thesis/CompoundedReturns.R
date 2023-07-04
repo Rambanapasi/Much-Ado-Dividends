d68260ca5e78531bcda83d4beab15a79e8332916
Rambanapasi <124045658+Rambanapasi@users.noreply.github.com>
2023-07-05 00:25:52 +0200

    Some indices are not shows returns returns despite the data being avaiable

---
+++ Thesis/CompoundedReturns.R
@@ -0,0 +1,77 @@
+if (!require("rmsfuns")) install.packages("rmsfuns")
+
+pacman::p_load("xts", "tidyverse", "tbl2xts", "PerformanceAnalytics", 
+               "lubridate", "glue")
+# load any data just amek sure that the date is in the first column, this will be foe easy use of the xts package
+
+data <- readxl::read_xlsx("data/MAD Dividend index series-2.xlsx")
+
+#  Mutate_at replaces all the previous columns
+d1 <- 
+  data %>% 
+  arrange(Date) %>% 
+  mutate_at(.vars = vars(-Date), ~log(.)-log(lag(.)))
+
+# Just to make some things much simplier
+d2 <- data %>% 
+  gather(Index, Value, -Date) %>% 
+  mutate(Return = Value/lag(Value)-1) %>% 
+  ungroup()
+# Lets understand the xts package and what we can do with it 
+
+#  Lets get log returns using the dplyr, no long format just yet
+
+Year_Month <- function(x) format(as.Date(x), "%Y_%B") 
+
+cumdata_Logret_Monthly <- 
+  cumdata %>% 
+  arrange(Date) %>% 
+  filter(Date >= lubridate::ymd(20130128)) %>% 
+  group_by(YM) %>% filter(Date == last(Date)) %>% 
+  ungroup() %>% 
+  mutate(across(.cols = where(is.numeric), .fns = ~log(.) - lag(log(.)))) %>% 
+  
+  filter(Date > first(Date)) %>% mutate(Year = format(Date, "%Y")) %>% 
+  
+  group_by(Year) %>% 
+  # Now we can sum to get to annual returns:
+  summarise(across(.cols = where(is.numeric), .fns = ~sum(.)))
+
+
+#  Now to do this using the tidy mannaer 
+
+d2 <- data %>% 
+  arrange(Date) %>% 
+  mutate(YM = Year_Month(Date)) %>% 
+  gather(index, value, -Date) %>% 
+  group_by(index) 
+
+#  Just converting it to be numeric
+
+d2$value <- as.numeric(d2$value)
+
+
+#  These are just the normal componded returns
+compounded_returns <- d2 %>%
+  mutate(YM = Year_Month(Date)) %>% 
+  mutate(dailyReturn = value/lag(value) - 1) %>% 
+  filter(Date >= lubridate::ymd(20130128)) %>% 
+  # coalesce returns NULL or NA 
+  mutate(daliyReturn = coalesce(dailyReturn, 0)) %>% 
+  mutate(dailyIndex = cumprod(1 + dailyReturn)) %>% 
+  group_by(YM) %>% 
+  filter(Date == last(Date)) %>% 
+  group_by(index) %>% 
+  mutate(MonthlyReturn = dailyIndex/lag(dailyIndex) - 1) %>% 
+  select(Date, index, dailyIndex)
+
+
+compounded_returns %>%
+  ggplot(aes(Date, dailyIndex, color = index, group = index)) +
+  geom_line() +
+  facet_wrap(~ index) +
+  theme(legend.position = "none")
+
+
+  
+
---
+++ Thesis/XTS.R
@@ -0,0 +1,56 @@
+data %>% names %>%  unique
+
+interest <- c("GDDUK", "M2JPDY")
+#  Some loadings 
+
+pacman::p_load("xts", "tidyverse", "tbl2xts", "PerformanceAnalytics", 
+               "lubridate", "glue", "extrafont")
+
+# The XTS package, its a complement to PerformanceAnalytics and 
+
+data <- readxl::read_xlsx("data/MAD Dividend index series-2.xlsx")
+
+#  Using the xts package to manipulate the data
+
+xts.data <- tbl_xts(data)
+
+covid_hyper_data <- xts.data["2019-3/2021-4"]
+
+# This package is very useful is subsetting dates, consider the following should consider deep analysis on certain securities
+
+xts.data[.indexwday(xts.data) == 1]  # Mondays only
+
+xts.data[.indexmon(xts.data) == 0]  # Date's in Jan in all years
+
+xts.data[.indexmday(xts.data) == 1]  # First day of every month
+
+xts.data[endpoints(xts.data, "months", k = 3)]  # Last day of every quarter (hence k = 3)
+
+
+# Plotting with default lables is simple
+
+
+plot(xts.data[, "FUDP"], major.ticks = "years", minor.ticks = FALSE, 
+     main = "", col = 3, ylab = "")
+
+#  If dates have no values which happens all the time the best thing to do is use the code below
+xts.data[150, "FUP"] <- NA
+
+
+#  Sometimes we have NA and we can just pad them which returns values of the previous trading day, in the code it gives a max of 5 days 
+
+xts.data.padded <- na.locf(xts.data, na.rm = F, maxgap = 10)
+
+# To get say quarterly dates for the securties we hold you easily just, 
+
+xts.data.padded.monthly <- xts.data.padded[endpoints(xts.data.padded, 
+                                                     "months", k = 3)]
+
+
+------------------
+  # The actual work 
+-------------------
+
+
+
+
