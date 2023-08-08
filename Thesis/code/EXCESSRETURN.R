1e796740906faf2ae7a6f0139390f78b382b0a5b
Rambanapasi <124045658+Rambanapasi@users.noreply.github.com>
2023-08-08 20:46:48 +0200

    Get all the excesses then comment

--- Thesis/code/EXCESSRETURN.R
+++ Thesis/code/EXCESSRETURN.R
@@ -1,26 +1,15 @@
-MY_excess_return <- function(df, index_name, benchmark_name) {
-  df = df %>%
-    rename(Index = "index_name", Benchmark = "benchmark_name") %>%
-    select(Date, Index, Benchmark) %>% 
-    mutate(Return = Index / lag(Index)) %>%
-    mutate(Return = replace(Return, is.na(Return), 0)) %>%
-    mutate(Excess_Return = Index - Benchmark) %>%
-    mutate(YM = format(index, "%Y-%b")) %>%
-    group_by(YM) %>%
-    filter(index == last(index)) %>%
-    mutate(Cumulative_Excess_Return = cumprod(1 + Excess_Return))
-  df
-}
-
-part  <-  function(df, index_name, benchmark_name) {
-  df = df %>%
+MY_excess_return <-  function(df, index_name, benchmark_name) {
+  df <-  df %>%
     dplyr::rename(Index = index_name, Benchmark = benchmark_name) %>% 
-    select(Date, Index, Benchmark) %>% gather(Vehicle, Value, -Date) %>% 
+    select(Date, Index, Benchmark) %>% gather(Vehicle, Value, -Date) %>%  
     mutate(Return = Value / lag(Value)-1) %>%
-    mutate(Return = coalesce(Return, 0)) %>%
-    tbl2xts::tbl_xts(.,cols_to_xts = Value, spread_by = Vehicle ) %>% 
-    tbl2xts::xts_tbl
+    mutate(Return = coalesce(Return, 0)) %>% 
+    tbl2xts::tbl_xts(.,cols_to_xts = Return, spread_by = Vehicle) %>% 
+    tbl2xts::xts_tbl() %>% 
+    mutate(ex.ret = Index - Benchmark) %>% 
+    mutate(cum_return = cumprod(1+ex.ret)) %>% 
+    select(date, cum_return)
   df
 }
 
-b <- df %>% part(., "FUDP", "M2EFDY")
+
