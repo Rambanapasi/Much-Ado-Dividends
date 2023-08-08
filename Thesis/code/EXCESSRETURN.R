2c8cdef0f5c95320a31f452f3db05e0ee1256d31
Rambanapasi <124045658+Rambanapasi@users.noreply.github.com>
2023-08-08 23:04:42 +0200

    working

--- Thesis/code/EXCESSRETURN.R
+++ Thesis/code/EXCESSRETURN.R
@@ -1,16 +1,16 @@
-MY_excess_return <-  function(df, index_name, benchmark_name) {
-  df <-  df %>%
+MY_excess_return <- function(df, index_name, benchmark_name) {
+  df <- df %>%
     dplyr::rename(Index = index_name, Benchmark = benchmark_name) %>% 
-    select(Date, Index, Benchmark) %>% gather(Vehicle, Value, -Date) %>%  
-    mutate(Return = Value / lag(Value)-1) %>%
+    select(Date, Index, Benchmark) %>% 
+    gather(Vehicle, Value, -Date) %>%  
+    mutate(Return = Value / lag(Value) - 1) %>%
     mutate(Return = coalesce(Return, 0)) %>% 
-    tbl2xts::tbl_xts(.,cols_to_xts = Return, spread_by = Vehicle) %>% 
-    tbl2xts::xts_tbl() %>% 
+    tbl_xts(., cols_to_xts = Return, spread_by = Vehicle) %>% 
+    xts_tbl() %>% 
     mutate(ex.ret = Index - Benchmark) %>% 
-    mutate(cum_return = cumprod(1+ex.ret)) %>% 
-    select(date, cum_return) %>% 
-    rename(., index_name = cum_return)
+    mutate(cum_return = cumprod(1 + ex.ret)) %>% 
+    select(date, cum_return)
+
+  colnames(df)[2] = index_name
   df
 }
-
-
